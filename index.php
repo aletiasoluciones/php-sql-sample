49bc301113f6e94a4ea05a03778557bd4465d4b3
Student User <student@workstation.lab.example.com>
2022-11-18 04:44:30 -0500

    fixed mysql class

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $password = getenv('DB_PASS');
 $dbname = getenv('DB_NAME');
 
 // Create connection
-$conn = new mysql($servername.':'.$serverport, $username, $password, $dbname);
+$conn = new mysqli($servername.':'.$serverport, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
